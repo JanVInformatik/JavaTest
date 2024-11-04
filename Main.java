ce42b3690370ec93152ca48ab0daf164b40482f7
JanV <janvogele2007@icloud.com>
2024-11-04 21:39:48 +0100

    Simple implementation of a binary search.

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Test");
+    // make a binary search
+    BinarySearch binarySearch = new BinarySearch();
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int target = 5;
+    int index = binarySearch.binarySearch(arr, target);
+    System.out.println("Index of " + target + " is: " + index);
   }
 }
